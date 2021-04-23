3adae2538bfee3e191ea5a806983bb5b6f74d6dd
Rick <real4ric@gmailo.com>
2021-04-23 13:39:39 +0600

    Reinitialize comments inside TS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,177 +1,147 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   public animalList = [
     {
       title: 'Vache',
       image: 'img/animals/cow-icon.png',
       desc: 'Meugle',
       file: '/sounds/cow.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Dauphin',
       image: 'img/animals/dolphin-icon.png',
       desc: 'Siffle',
       file: '/sounds/dolphin.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Grenouille',
       image: 'img/animals/frog-icon.png',
       desc: 'Coasse',
       file: '/sounds/frog.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Oiseau',
       image: 'img/animals/bird-icon.png',
       desc: 'Chante',
       file: '/sounds/bird.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Cochon',
       image: 'img/animals/pig-icon.png',
       desc: 'Grogne',
       file: '/sounds/pig.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Chien',
       image: 'img/animals/puppy-icon.png',
       desc: 'Aboie',
       file: '/sounds/dog.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Chat',
       image: 'img/animals/black-cat-icon.png',
       desc: 'Miaule',
       file: '/sounds/cat.mp3',
-      playing: false
+      playing: false,
     },
     {
       title: 'Cheval',
       image: 'img/animals/horse-icon.png',
       desc: 'Hennit',
       file: '/sounds/horse.wav',
-      playing: false
+      playing: false,
     },
     {
       title: 'Ane',
       image: 'img/animals/donkey-icon.png',
       desc: 'Brait',
       file: '/sounds/donkey.wav',
-      playing: false
-    }
+      playing: false,
+    },
   ];
 
-  //## 1 stockagede l'animal choisi 
-  public chosenAnimal = null;
-
-  //10 Time maximum to reset
-  private answerDelayInSecond = 30;
-  //10 Time left to reset
-  public secondLeft = null;
-
-  //10 le chrono pour le reponse
-  private timer = null;
-
-  ////## 2 stockage d'un objet audio HTML
-  private audio: HTMLAudioElement = null;
-
-  constructor(private toastCtrl: ToastController) { }  //## 4 Ask for ToastController 
-
-  public play() {
-    //## 1 Choisir un animal au hasard  
-    let isRandom = Math.floor(Math.random() * this.animalList.length);  //Math.floor to go for nearest full number and Math.random makes things random
-
+  
+  public chosenAnimal = null;                        //## 1 stockagede l'animal choisi
+  private answerDelayInSecond = 30;                  //10 Time maximum to reset
+  public secondLeft = null;                          //10 Time left to reset
+  private timer = null;                               //10 le chrono pour le reponse
+  private audio: HTMLAudioElement = null;             //## 2 stockage d'un objet audio HTML
+  public tries = 0;                                   //# 11 Nombre de coups
+  private maxTries = 3;
+  constructor(private toastCtrl: ToastController) {}                      //## 4 Ask for ToastController
+  
+  public play() {                                                         //## 1 Choisir un animal au hasard   
+    let isRandom = Math.floor(Math.random() * this.animalList.length);      //Math.floor to go for nearest full number and Math.random makes things random
     this.chosenAnimal = this.animalList[isRandom];
 
-    // ## 3 To eviter sound mixing, (arreter le son precedent)
-    if (this.audio && !this.audio.ended) {
+     if (this.audio && !this.audio.ended) {                                  // ## 3 To eviter sound mixing, (arreter le son precedent)
       this.audio.pause();
     }
-
-    //## 10 Lancement du chrono
-    this.startTimer(); 
-
-
-    //## 2 Jouer un son  
-    //instanciation d'un objet audio avec le son que l'on veut jouer
-    this.audio = new Audio('/assets' + this.chosenAnimal.file)
-    //chargement du son
-    this.audio.load();
-    // lecture du son
-    this.audio.play();
+    this.startTimer();                                                //## 10 Lancement du chrono
+    this.audio = new Audio('/assets' + this.chosenAnimal.file);         //## 2 Jouer un son //instanciation d'un objet audio avec le son que l'on veut jouer
+    this.audio.load();                                                //chargement du son
+    this.audio.play();                                                  // lecture du son
   }
 
-  //## 4 Choix du joueur  
-  public async guessAnimal(clickedAnimal) {
-
-    // ## 5 si aucun son n'a ete joue_ chosenAnimal est null  
-    if (this.chosenAnimal == null) {
+  
+  public async guessAnimal(clickedAnimal) {                         //## 4 Choix du joueur
+    if (this.chosenAnimal == null) {                                 // ## 5 si aucun son n'a ete joue_ chosenAnimal est null
       return;
     }
-    // dECLARE VARIABLES
-    let message;
-    let toastColor = 'danger'; //## 8
-    
+    this.tries++;
+    let message;                                // dECLARE VARIABLES
+    let toastColor = 'danger';                  //## 8
 
-    // ## 4 comparison des animaux  
-    //celui sur lequel le jouer a clique
-    //et celui dont on a joue le son
-    if (this.chosenAnimal.title == clickedAnimal.title) {
-      message = "Win!!!";
-      toastColor = 'secondary';
-      this.resetGame(); //## 9
-    } else {
-      message = "try again";
+    if(this.tries > this.maxTries) {             //## 12 max tries
+      message = "Vous avez depasse le nombre de coups autorise";
+      toastColor = "warning";
+      this.resetGame();
     }
-
-    // ## 8 Affichage du message  (async-await)
-    const toast = await this.toastCtrl.create({
+    else if (this.chosenAnimal.title == clickedAnimal.title) {          // ## 4 comparison des animaux //celui sur lequel le jouer a clique //et celui dont on a joue le son
+      message = 'gagne en ' + this.tries + ' coups';                  //win
+      toastColor = 'primary';
+      this.resetGame();                                                 //## 9
+    } else {
+      message = 'try again';
+    }
+    const toast = await this.toastCtrl.create({                         // ## 8 Affichage du message  (async-await)
       message: message,
       duration: 1000,
       position: 'top',
-      color: toastColor  //## 8
+      color: toastColor,                                                  //## 8
     });
 
     toast.present();
   }
-
- //## 9 Reset game if win
-  private resetGame(){
+  
+  private resetGame() {                                 //## 9 Reset game if win
     this.chosenAnimal = null;
     this.audio = null;
-
-    this.secondLeft = 0; //## 10
-    clearInterval(this.timer)
-  }
-
-  //10 Set Timer to show time left and a countdown reverse
-  private startTimer(){
+    this.tries = 0;
+    this.secondLeft = 0;                                //## 10
+    clearInterval(this.timer);
+  } 
+    private startTimer() {                                  //10 Set Timer to show time left and a countdown reverse
     this.secondLeft = this.answerDelayInSecond;
-    this.timer = setInterval(()=>{
-      //Decremente le temps restants
-      this.secondLeft--; 
-      //s'il ne reste plus de temps, on remet le jeu a zero
-      if (this.secondLeft == 0) {
+    this.timer = setInterval(() => { 
+      this.secondLeft--;                                  //Decremente le temps restants
+      if (this.secondLeft == 0) {                           //s'il ne reste plus de temps, on remet le jeu a zero 
         this.resetGame();
-        //annulation du chrono
-        clearInterval(this.timer);
+        clearInterval(this.timer);                            //annulation du chrono
       }
-    },1000)
+    }, 1000);
   }
-
-
 }
